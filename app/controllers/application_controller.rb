c9c79f0334bcc81f155b77774de27d0fadda22bb
Trong Thao <trongthaonh@gmail.com>
2015-08-02 22:26:48 +0700

    Apply strong_parameters filtering before CanCan authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,17 @@ class ApplicationController < ActionController::Base
   #Allow controllers to handle token authentication
   acts_as_token_authentication_handler_for User
   
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # Apply strong_parameters filtering before CanCan authorization
+  # See https://github.com/ryanb/cancan/issues/571#issuecomment-10753675
+  before_filter do
+    resource = controller_name.singularize.to_sym
+    method = "#{resource}_params"
+    params[resource] &&= send(method) if respond_to?(method, true)
+  end
+  
+  rescue_from CanCan::AccessDenied do |exception|  
+    respond_to do |format|  
+      format.json { render :json=> exception.to_json, :status => :forbidden }  
+    end   
+  end
 end
