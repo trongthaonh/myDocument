1a5bc50caa297424d976c710f43decf688dfe218
Trong Thao <trongthaonh@gmail.com>
2015-08-02 15:11:05 +0700

    Allow controllers to handle token authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   protect_from_forgery with: :null_session, :if => Proc.new { |c| c.request.format == 'application/json' }
+  
+  #Allow controllers to handle token authentication
+  acts_as_token_authentication_handler_for User
+  
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
