0413a3b035723e68aac2d598b18ad6011bcc0f43
Trong Thao <trongthaonh@gmail.com>
2015-08-09 17:33:30 +0700

    Loading carrierwave after loading ORM

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#Make sure loading carrierwave after loading ORM
+require 'carrierwave/orm/activerecord'
+require "carrierwave/storage/abstract"
+require "carrierwave/storage/file"
