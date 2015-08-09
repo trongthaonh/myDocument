1e05498b68cc35060090743e3762d16f3bfbb582
Trong Thao <trongthaonh@gmail.com>
2015-08-09 10:18:20 +0700

    Catch error when set_project

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -51,7 +51,12 @@ class API::V1::ProjectsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_project
-      @project = Project.find(params[:id])
+      begin
+        @project = Project.find(params[:id])
+      rescue Exception => e
+        @error = e.message
+        render :json => {:error => @error}
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
