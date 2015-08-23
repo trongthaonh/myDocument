0da09724667aff049e73e459707e78ac3e6b192b
Trong Thao <trongthaonh@gmail.com>
2015-08-23 23:31:02 +0700

    Config mailer for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,21 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Config default url mailer action
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.default_url_options = { :host => 'sv1.trongthao.me' }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "info.mydocumentvn@gmail.com",
+    password: "!@##@!!@##@!"
+  }  
 end
